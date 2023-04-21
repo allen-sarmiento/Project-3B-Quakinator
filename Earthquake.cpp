57256ada4c74cb034dbe051564bca7bdabf52db1
mariajaureguiim <maria.jaureguiim@ufl.edu>
2023-04-21 18:50:05 -0400

    hi

---
+++ Earthquake.cpp
@@ -0,0 +1,61 @@
+#include "Earthquake.h"
+#pragma once
+#include <string>
+#include <vector>
+#include <iostream>
+using std namespace ;
+
+void Earthquake::swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+bool Earthquake:: compareByTime(const Earthquake& a, const Earthquake& b) {
+    return a.time < b.time;
+}
+
+bool Earthquake::compareByMagnitude(const Earthquake& a, const Earthquake& b) {
+    return a.magnitude < b.magnitude;
+}
+
+bool Earthquake::compareByLongitude(const Earthquake& a, const Earthquake& b) {
+    return a.longitude < b.longitude;
+}
+
+bool Earthquake::compareByLatitude(const Earthquake& a, const Earthquake& b) {
+    return a.latitude < b.latitude;
+}
+
+int Earthquake::partition(vector<Earthquake>& array, int low, int high, bool (*compare)(const Earthquake&, const Earthquake&)) {
+    Earthquake pivot = array[low];
+    int up = low
+    int down = high;
+
+    while (up < down) {
+        for (int j = up; j < high; j++) {
+            if (compare(array[j], pivot)) {
+                break;
+            }
+            up++;
+        }
+        for (int j = high; j > low; j--) {
+            if (!compare(array[j], pivot)) {
+                break;
+            }
+            down--;
+        }
+        if (up < down) {
+            swap(array[up], array[down]);
+        }
+    }
+    swap(array[low], array[down]);
+    return down;
+}
+
+void Earthquake::quickSort(vector<Earthquake>& array, int low, int high, bool (*compare)(const Earthquake&, const Earthquake&)) {
+    if (low < high) {
+        int pivot = partition(array, low, high, compare);
+        quickSort(array, low, pivot - 1, compare);
+        quickSort(array, pivot + 1, high, compare);
+    }
+}
--- Earthquake.h
+++ Earthquake.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <vector>
+#include <string>
+#include <iostream>
 
-using namespace std;
-
-class Earthquake {
-    private:
-    public:
-
+struct Earthquake {
+    int  time;
+    double magnitude;
+    double longitude;
+    double latitude;
 };
+void swap(int *a, int *b);
+bool compareByTime(const Earthquake& a, const Earthquake& b);
+bool compareByMagnitude(const Earthquake& a, const Earthquake& b);
+bool compareByLongitude(const Earthquake& a, const Earthquake& b);
+bool compareByLatitude(const Earthquake& a, const Earthquake& b);
+
+int partition(std::vector<Earthquake>& array, int low, int high, bool (*compare)(const Earthquake&, const Earthquake&));
+void quickSort(std::vector<Earthquake>& array, int low, int high, bool (*compare)(const Earthquake&, const Earthquake&));
