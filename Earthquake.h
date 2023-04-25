306410d86845ec7a6caf5a238d56bc10e3b74bf6
mariajaureguiim <maria.jaureguiim@ufl.edu>
2023-04-25 01:11:38 -0400

    fixed sort funtion

--- Earthquake.h
+++ Earthquake.h
@@ -14,9 +14,9 @@ struct Earthquake {
     Earthquake(string sTime, double magnitude, double longitude, double latitude);
 
     void swap(int *a, int *b);
-    bool compareByTime(const Earthquake& a, const Earthquake& b);
-    bool compareByMagnitude(const Earthquake& a, const Earthquake& b);
-    bool compareByLongitude(const Earthquake& a, const Earthquake& b);
-    bool compareByLatitude(const Earthquake& a, const Earthquake& b);
+    static bool compareByTime(const Earthquake& a, const Earthquake& b);
+  static  bool compareByMagnitude(const Earthquake& a, const Earthquake& b);
+   static bool compareByLongitude(const Earthquake& a, const Earthquake& b);
+   static bool compareByLatitude(const Earthquake& a, const Earthquake& b);
     string to_string();
 };
